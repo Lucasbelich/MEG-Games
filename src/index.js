87f757484281d44ddc38102fa32dc38887904e6b
Lucasbelich <lucasbelich1@gmail.com>
2022-07-27 22:46:45 -0300

    styles

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App baseName="/MEG-Games" />
   </React.StrictMode>,
   document.getElementById('root')
 );
